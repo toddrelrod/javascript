f2179822d4da53aec0525f7b515f38fb4118cbfb
Todd Elrod <tre@Todds-MacBook-Pro.local>
2019-12-12 20:57:05 -0800

    added javascript, numbers, strings, arrays, and objectst

---
+++ JS/scripts.js
@@ -0,0 +1,41 @@
+// Numbers
+// numbers and be computed with +, -, *, ?, %, ++ increment, --decrement
+// NaN means Not a Number
+var pizza = 5;
+var soda = 1;
+var bread = 3;
+
+var total=pizza+soda
+
+console.log (total)
+
+/**
+ * the consol. log() is a function in JavaScript which is used to print any kind of variables defined before in it or to just print any message that needs to be displayed to the user.
+ */
+
+//   String
+
+const name= 'Todd Elrod';
+
+const welcome='Welcome to Geekwise Academy';
+
+console.log(name)
+console.log(welcome)
+
+// Array
+var fruits=['apples','grapes','oranges','bananas'];
+var rockBand=['Pantera','Kiss','Jonas Brothers','Tool','Nickleback'];
+
+console.log(fruits);
+console.log(rockBand);
+
+var car={
+    type:"Fiat",
+    model:"500",
+    color:"white",
+};
+
+console.log(car);
+
+// Boolean
+// Everything with a real value is true 0, null, undefined is false.
