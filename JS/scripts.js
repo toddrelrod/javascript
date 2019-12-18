ad5a57d1f526e78f51b26ada399beb078757a57d
Todd Elrod <tre@Todds-MacBook-Pro.local>
2019-12-17 21:07:54 -0800

    added javascript button and favorite movie

--- JS/scripts.js
+++ JS/scripts.js
@@ -38,4 +38,53 @@ var car={
 console.log(car);
 
 // Boolean
-// Everything with a real value is true 0, null, undefined is false.
+// everything with a real value is true 0, null, undefined is false
+// comparison operators always return true or false ==, ===, !=, !==, !===, <, >, <=, >=, && AND, || OR, ! NOT
+
+const present = true;
+const absent = false;
+Boolean(present);
+
+console.log(present);
+
+// Undefined
+
+var cat = 
+
+console.log(cat);
+
+// Null
+var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+person = null;   
+ // Now value is null, but type is still an object
+console.log(person);
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button 
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+var favoritMovie = 'My favorit movie is Brave Heart'
+
+document.write(favoritMovie);
+
+var mybutton = document.getElementById("myBtn");
+
+window.onmouseover = function() {randomMotion()};
+function randomMotion() {
+    mybutton.style.left =(Math.random()*800)+"px";
+    mybutton.style.top =(Math.random()*800)+"px"; 
+  }
+// // command question mark is a single line comment
